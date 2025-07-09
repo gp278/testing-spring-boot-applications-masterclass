ad7fe4755abd5733339175f40d0d3780b1bc6748
gudrunpoetzelberger <g.poetzelberger@easy-connect.io>
2025-07-09 10:20:07 +0200

    JUnit 5 and Mockito tests

--- src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
+++ src/test/java/de/rieckpil/courses/book/management/BookSynchronizationListenerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,14 +23,45 @@ class BookSynchronizationListenerTest {
   @Captor private ArgumentCaptor<Book> bookArgumentCaptor;
 
   @Test
-  void shouldRejectBookWhenIsbnIsMalformed() {}
+  void shouldRejectBookWhenIsbnIsMalformed() {
+    final String invalidIsbn = "123456789012";
+    BookSynchronization bookSynchronization = new  BookSynchronization(invalidIsbn);
+    cut.consumeBookUpdates(bookSynchronization);
+    verifyNoInteractions(openLibraryApiClient, bookRepository);
+  }
 
   @Test
-  void shouldNotOverrideWhenBookAlreadyExists() {}
+  void shouldNotOverrideWhenBookAlreadyExists() {
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(new  Book());
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    cut.consumeBookUpdates(bookSynchronization);
+    verify(bookRepository, never()).save(any());
+    verifyNoInteractions(openLibraryApiClient);
+  }
 
   @Test
-  void shouldThrowExceptionWhenProcessingFails() {}
+  void shouldThrowExceptionWhenProcessingFails() {
+    BookSynchronization bookSynchronization = new BookSynchronization(VALID_ISBN);
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenThrow(new RuntimeException("Network Timeout"));
+    assertThrows(RuntimeException.class, () -> cut.consumeBookUpdates(bookSynchronization));
+  }
 
   @Test
-  void shouldStoreBookWhenNewAndCorrectIsbn() {}
+  void shouldStoreBookWhenNewAndCorrectIsbn() {
+    Book book = new Book();
+    book.setIsbn(VALID_ISBN);
+    book.setTitle("Java Book");
+    when(bookRepository.findByIsbn(VALID_ISBN)).thenReturn(null);
+    when(openLibraryApiClient.fetchMetadataForBook(VALID_ISBN)).thenReturn(book);
+    BookSynchronization bookSynchronization = new  BookSynchronization(VALID_ISBN);
+
+    cut.consumeBookUpdates(bookSynchronization);
+
+    Mockito.verify(openLibraryApiClient, times(1)).fetchMetadataForBook(VALID_ISBN);
+    Mockito.verify(bookRepository, times(1)).save(bookArgumentCaptor.capture());
+    verifyNoMoreInteractions(openLibraryApiClient, bookRepository);
+    assertEquals("Java Book", bookArgumentCaptor.getValue().getTitle());
+    assertEquals(VALID_ISBN, bookArgumentCaptor.getValue().getIsbn());
+  }
 }
--- src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
+++ src/test/java/de/rieckpil/courses/book/review/ReviewVerifierTest.java
@@ -1,5 +1,8 @@
 package de.rieckpil.courses.book.review;
 
+import org.assertj.core.api.Assertions;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
@@ -8,8 +11,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
+import java.util.List;
+
 import static de.rieckpil.courses.book.review.RandomReviewParameterResolverExtension.RandomReview;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @ExtendWith(RandomReviewParameterResolverExtension.class)
 class ReviewVerifierTest {
@@ -32,21 +38,57 @@ class ReviewVerifierTest {
 
   @Test
   @DisplayName("Should fail when review contains 'lorem ipsum'")
-  void testLoremIpsum() {}
+  void testLoremIpsum() {
+    String review = "bla Lorem Ipsum Dolor";
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect lorem ipsum");
+  }
 
   @ParameterizedTest
   @CsvFileSource(resources = "/badReview.csv")
-  void shouldFailWhenReviewIsOfBadQuality(String review) {}
+  void shouldFailWhenReviewIsOfBadQuality(String review) {
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect bad review");
+  }
 
   @RepeatedTest(5)
-  void shouldFailWhenRandomReviewQualityIsBad(@RandomReview String review) {}
+  void shouldFailWhenRandomReviewQualityIsBad(@RandomReview String review) {
+    boolean result = reviewVerifier.doesMeetQualityStandards(review);
+    assertFalse(result, "ReviewVerifier did not detect bad review");
+  }
 
   @Test
-  void shouldPassWhenReviewIsGood() {}
+  void shouldPassWhenReviewIsGood() {
+    String goodReview = "This is a good review. It has no swear words and enough words and so on.";
+    boolean result = reviewVerifier.doesMeetQualityStandards(goodReview);
+    assertTrue(result, "ReviewVerifier did not detect bad review");
+  }
 
   @Test
-  void shouldPassWhenReviewIsGoodHamcrest() {}
+  void shouldPassWhenReviewIsGoodHamcrest() {
+    String goodReview = "This is a good review. It has no swear words and enough words and so on.";
+    boolean result = reviewVerifier.doesMeetQualityStandards(goodReview);
+    MatcherAssert.assertThat("ReviewVerifier did not detect bad review", result, Matchers.equalTo(true));
+    MatcherAssert.assertThat("Lorem Ipsum", Matchers.endsWith("psum"));
+    MatcherAssert.assertThat(List.of(1,2,3,4,5), Matchers.hasSize(5));
+    MatcherAssert.assertThat(List.of(1,2,3,4,5), Matchers.anyOf(Matchers.hasSize(5), Matchers.emptyIterable()));
+    MatcherAssert.assertThat(List.of(1,2,3,4,5), Matchers.allOf(Matchers.hasSize(5), Matchers.notNullValue()));
+  }
 
   @Test
-  void shouldPassWhenReviewIsGoodAssertJ() {}
+  void shouldPassWhenReviewIsGoodAssertJ() {
+    String goodReview = "This is a good review. It has no swear words and enough words and so on.";
+    boolean result = reviewVerifier.doesMeetQualityStandards(goodReview);
+    Assertions.assertThat(result)
+      .withFailMessage("ReviewVerifier did not detect bad review")
+      .isEqualTo(true)
+      .isTrue();
+
+    Assertions.assertThat(List.of(1,2,3,4,5))
+      .hasSizeBetween(2,10)
+      .contains(3)
+      .isNotEmpty()
+    ;
+
+  }
 }
